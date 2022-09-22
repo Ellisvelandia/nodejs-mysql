43f6ae9586f405f5ea2976dc3e67d7de5fed9992
ellisvelandia <eyis619@gmail.com>
2022-09-21 21:54:40 -0500

    mysql

--- src/index.js
+++ src/index.js
@@ -7,7 +7,13 @@ const app = express();
 app.use(express.json());
 
 app.use(indexRoutes);
-app.use('/api',  employeesRoutes);
-app.listen(3000);
+app.use("/api", employeesRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: "endpoint not found",
+  });
+});
+
+app.listen(3000);
 console.log("Server running on port 3000");
