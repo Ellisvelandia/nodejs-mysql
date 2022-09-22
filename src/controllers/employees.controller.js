7b19f65a219727c1fb11415102c2d8eeeb8eab1c
ellisvelandia <eyis619@gmail.com>
2022-09-21 20:34:30 -0500

    mysql

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -5,6 +5,13 @@ export const getEmployees = async (req, res) => {
    res.json(rows)
 };
 
+export const getEmployee = async (req, res ) => {
+  console.log(req.params.id)
+  const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [req.params.id])
+  console.log(rows)
+  res.json(rows[0])
+}
+
 export const createEmployee = async (req, res) => {
   const { name, salary } = req.body
   const [rows] = await pool.query("INSERT INTO employee(name,salary) VALUES (?, ?)",
--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -4,11 +4,14 @@ import {
   createEmployee,
   updateEmployee,
   deleteEmployee,
+  getEmployee,
 } from "../controllers/employees.controller.js";
 
 const router = Router();
 
 router.get("/employees", getEmployees);
+
+router.get("/employees/:id", getEmployee);
 
 router.post("/employees", createEmployee);
 
