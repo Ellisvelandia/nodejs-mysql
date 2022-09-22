6ce24bb36d6f74d84bd37eebbef5a1c25d2de1ea
ellisvelandia <eyis619@gmail.com>
2022-09-21 21:25:20 -0500

    mysql

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -41,7 +41,15 @@ export const deleteEmployee = async (req, res) => {
 export const updateEmployee = async (req, res) => {
   const {id} = req.params
   const {name, salary} = req.body
-const [result] = await pool.query('UPDATE employee SET name = ?, salary = ? WHERE id = ?', [name, salary, id])
+const [result] = await pool.query('UPDATE employee SET name = IFNULL(?, name), salary = IFNULL (?, salary) WHERE id = ?', [name, salary, id])
+
+if (result.affectedRows === 0 ) return res.status(404).json({
+  message: 'Employee not found'
+})
+
 console.log(result)
-res.json('received')
+
+const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [id])
+
+res.json(rows[0]) 
 };
